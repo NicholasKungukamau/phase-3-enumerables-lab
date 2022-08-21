d569be171753d77db30610a11c83dd07e7d8f11e
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-08-21 18:23:59 +0300

    commit tree 9111200a9ec8f04a89be502e145764349b727a64

--- enumerables.rb
+++ enumerables.rb
@@ -13,12 +13,14 @@ end
 # with the names of each spicy food
 def get_names(spicy_foods)
   # your code here
+    spicy_foods.map { |obj| obj[:name] }
 end
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
   # your code here
+    spicy_foods.filter { |obj| obj[:heat_level] > 5 }
 end
 
 # given an array of spicy foods, **output to the terminal**
@@ -28,18 +30,23 @@ end
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
   # your code here
+   spicy_foods.each do |obj|
+    puts "#{obj[:name]} (#{obj[:cuisine]}) | Heat Level: #{"🌶" * obj[:heat_level]}"
+  end
 end
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
   # your code here
+    spicy_foods.find { |obj| obj[:cuisine] == cuisine }
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
 # sorted by heat level from lowest to highest
 def sort_by_heat(spicy_foods)
   # your code here
+    spicy_foods.sort { |a, b| a[:heat_level] <=> b[:heat_level] }
 end
 
 # given an array of spicy foods, output to the terminal ONLY 
@@ -48,10 +55,15 @@ end
 # HINT: Try to use methods you've already written to solve this!
 def print_spiciest_foods(spicy_foods)
   # your code here
+   foods = spiciest_foods(spicy_foods)
+  foods.each do |food|
+    puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{"🌶" * food[:heat_level]}"
+  end
 end
 
 # given an array of spicy foods, return an integer representing 
 # the average heat level of all the spicy foods in the array
 def average_heat_level(spicy_foods)
   # your code here
+    spicy_foods.sum { |obj| obj[:heat_level] } / spicy_foods.length
 end
